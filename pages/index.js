0355d08ee0ffdaad8a3816213397783676b6b125
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2022-06-14 15:51:40 +0300

    Retrieve latest shots from Dribbble

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Head from 'next/head'
 
-export default () => {
+export default (dribbbleShots) => {
+
+	console.log(dribbbleShots)
+
 	return (
-		<div>
+		<div className='container'>
 			<Head>
 				<title>Corneliu Cîrlan</title>
 			</Head>
 
-			<div>Index</div>
+			<a className='btn-link'>Index</a>
 		</div>
 	)
 }
 
+export async function getStaticProps() {
+
+	// Get latest shots from Dribbble
+	let response = await fetch(`https://api.dribbble.com/v2/user/shots?access_token=${process.env.DRIBBBLE_TOKEN}&per_page=3`)
+	let posts = await response.json()
+
+	// Return latest shots
+	return {props: {posts: posts}}
+}
