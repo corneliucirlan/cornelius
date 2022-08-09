f75648724ea8e7cef4c73fb1079d11e088c63479
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2022-08-09 15:36:02 +0300

    Working contact form

--- pages/contact.js
+++ pages/contact.js
@@ -9,25 +9,75 @@ import styles from '../sass/modules/Contact.module.sass'
 
 export default () => {
 
+	// Form inputs states
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 	const [service, setService] = useState('')
 	const [budget, setBudget] = useState('')
 	const [message, setMessage] = useState('')
 
+	// Form inputs errors
 	const [errors, setErrors] = useState({})
 
-	useEffect(() => {
-		console.log('Errors: ', errors)
-	})
+	// useEffect(() => {
+	// 	console.log('Errors: ', errors)
+	// })
 
 	const handleSubmit = async (event) => {
-
+		
 		// Prevent page reload
 		event.preventDefault()
 
+		// Get submit button
+		let submitButton = document.querySelector('button.btn-form-submit')
+
+		// Get button inner HTML
+		const submitButtonDefault = submitButton.innerHTML
+
+		// Sending button HTML
+		const buttonIsSending = `
+			<div class='loader svg-icon'>
+				<div class='circle'></div>
+				<div class='ring'></div>
+			</div>
+			<span>Sending ...</span>`
+
+		// Success button HTML
+		const buttonSuccess = `
+			<svg class='svg-icon' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+				<path class='path' d="M18,8A10,10,0,1,0,28,18,10,10,0,0,0,18,8Zm0,1.935A8.065,8.065,0,1,1,9.935,18,8.06,8.06,0,0,1,18,9.935m5.653,5.253-.909-.916a.484.484,0,0,0-.684,0l-5.7,5.654L13.95,17.493a.484.484,0,0,0-.684,0l-.916.909a.484.484,0,0,0,0,.684l3.661,3.69a.484.484,0,0,0,.684,0l6.959-6.9A.484.484,0,0,0,23.653,15.188Z" transform="translate(-8 -8)"/>
+			</svg>
+			<span>Message sent</span>`
+
+		// Error button state
+		const buttonError = `
+			<svg class='svg-icon' xmlns="http://www.w3.org/2000/svg" width="20" height="17.778" viewBox="0 0 20 17.778">
+				<path class='path' d="M19.775,15.278a1.668,1.668,0,0,1-1.444,2.5H1.668a1.668,1.668,0,0,1-1.444-2.5L8.556.833a1.668,1.668,0,0,1,2.887,0l8.331,14.445ZM10,12.292a1.6,1.6,0,1,0,1.6,1.6A1.6,1.6,0,0,0,10,12.292ZM8.483,6.55l.258,4.722a.417.417,0,0,0,.416.394h1.686a.417.417,0,0,0,.416-.394l.258-4.722a.417.417,0,0,0-.416-.439H8.9a.417.417,0,0,0-.416.439Z" transform="translate(0)"/>
+			</svg>
+			<span>Something's wrong, try again in a bit</span>`
+		
+		// Reset button animation
+		const resetAnimation = () => {
+			submitButton.classList.add('btn-animate')
+			setTimeout(() => {
+				submitButton.classList.remove('btn-animate')
+			}, 200)
+		}
+
+		// Update button
+		const updateSubmitButton = (innerHTML, removeClass, addClass, disabled = true) => {
+			submitButton.innerHTML = innerHTML
+			disabled ? submitButton.setAttribute('disabled', true) : submitButton.removeAttribute('disabled')
+			removeClass && submitButton.classList.remove(...removeClass)
+			addClass && submitButton.classList.add(addClass)
+			resetAnimation()
+		}
+
 		// Send email if all fields are validated
 		if (!handleValidation()) {
+
+			// Button sending state
+			updateSubmitButton(buttonIsSending, '', '')
 
 			const res = await fetch("/api/sendgrid", {
 				body: JSON.stringify({
@@ -42,10 +92,11 @@ export default () => {
 				},
 				method: "POST",
 			})
-	
+
+			// Get response from server
 			let {code, responseMessage} = await res.json()
 
-			// Email sent, reset inputs
+			// // Email sent, reset inputs
 			if (code == 200) {
 				setName('')
 				setEmail('')
@@ -53,9 +104,19 @@ export default () => {
 				setBudget('')
 				setMessage('')
 
+				// Button success state
+				updateSubmitButton(buttonSuccess, 'btn-sending', 'btn-success')
+			} else {
 				console.log(code, responseMessage)
-				return
+
+				// Button error state
+				updateSubmitButton(buttonError, 'btn-success', 'btn-error')
 			}
+
+			// Reset button to default
+			setTimeout(() => {
+				updateSubmitButton(submitButtonDefault, ['btn-sending', 'btn-success', 'btn-error'], false, false)
+			}, 3000)
 		}
 	}
 
@@ -136,7 +197,7 @@ export default () => {
 
 			<Header />
 
-			<main className={`row d-flex align-items-center justify-content-center ${styles.contact}`}>
+			<main className={`row align-items-center ${styles.contact}`}>
 
 				<div className='col-12 col-md-5'>
 					<h4 className='text-uppercase'>Contact</h4>
@@ -146,7 +207,7 @@ export default () => {
 					<Button classes=' btn-footer btn-email-me' href='mailto:corneliu@corneliucirlan.com' text='corneliu@corneliucirlan.com' icon={true} />
 				</div>
 
-				<div className='col-12 col-md-5 offset-md-1'>
+				<div className='col-12 col-md-6 offset-md-1'>
 					<form action='' method='post' className='row' onSubmit={handleSubmit}>
 
 						{/* Name */}
@@ -241,14 +302,10 @@ export default () => {
 								></textarea>
 						</div>
 
-						<Button classes=' btn-footer btn-email-me btn-form-submit' href='#' text='Send message' icon={true} submit={true} />
-
-						{/* <div className='loader-container'>
-							<div className='circle'></div>
-							<div className='ring'></div>
-							<div className='ring'></div>
-						</div> */}
-						
+						{/* Submit button */}
+						<div className='col-12'>
+							<Button classes=' btn-footer btn-form-submit' href='#' text='Send message' icon={true} submit={true} />						
+						</div>
 					</form>
 				</div>
 			</main>
