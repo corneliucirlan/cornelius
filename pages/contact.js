5547ed2a62c3f84afa3dcb3b9f4d074d9cdee9ef
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2022-10-01 17:37:15 +0300

    Removed unused import

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,5 @@
 import { useState, useRef } from 'react'
 import parse from 'html-react-parser'
-import { useIsInViewport } from '../utils/transitions'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
