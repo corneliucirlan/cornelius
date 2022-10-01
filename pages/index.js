c0809ff9c6d9af4f1bdd50dc74f5b117a09b1c9a
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2022-10-01 17:35:28 +0300

    Streamlined index page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,115 @@
 import Head from 'next/head'
 
+import getPosts from '../components/get-posts'
 import Header from '../components/header'
-import Card from '../components/card'
 import Footer from '../components/footer'
-import Button from '../components/button'
-import Title from '../components/title'
+import Hero from '../components/frontpage/hero'
+import ListProjects from '../components/frontpage/list-projects'
 import LetsWorkTogether from '../components/work-together'
+import { caseStudies, personalProjects } from '../components/data/projects'
 
 import styles from '../sass/modules/Index.module.sass'
-import { setTransition } from '../utils/transitions'
-import { useRef, useEffect } from 'react'
-import { animated } from 'react-spring'
+import { getPhotoData } from '../utils/images'
 
-export default ({ dribbblePosts, igPosts }) => {
 
-	const heroRef = useRef()
+export const getServerSideProps = async () => {
+
+	// Case Studies posts
+	let studies = await Promise.all(caseStudies.map( async study => {
+		const post = {}
+		post.image = await getPhotoData(study.imageURL)
+
+		post.title = study.title
+		post.caption = study.caption
+		post.permalink = study.permalink
+		post.classes = [ 'col-12', 'col-md-6', 'card', 'card-dribbble' ]
+		post.source = null
+
+		return post
+	}))
+
+	// Personal Projects posts
+	let projects = await Promise.all(personalProjects.map( async study => {
+		const post = {}
+		post.image = await getPhotoData(study.imageURL)
+
+		post.title = study.title
+		post.caption = study.caption
+		post.permalink = study.permalink
+		post.classes = [ 'col-12', 'col-md-6', 'card', 'card-dribbble' ]
+		post.source = null
+
+		return post
+	}))
+	
+	// Get latest Dribbble posts
+	let dribbbleResult = await getPosts('https://api.dribbble.com/v2/user/shots', {
+		access_token: process.env.DRIBBBLE_TOKEN,
+		per_page: 2
+	})
+	
+	const dribbblePosts = await Promise.all(dribbbleResult.map(async dribbbleItem => {
+		const post = {}
+		post.image = await getPhotoData(dribbbleItem.images.hidpi)
+
+		post.title = dribbbleItem.title
+		post.caption = dribbbleItem.description
+		post.permalink = dribbbleItem.html_url
+		post.classes = [ 'col-12', 'col-md-6', 'card', 'card-dribbble' ]
+		post.source = 'dribbble'
+
+		return post
+	}))
+
+	// Get latest IG posts
+	let instagramResult = await getPosts('https://graph.instagram.com/v14.0/me/media', {
+		access_token: process.env.INSTAGRAM_TOKEN,
+		fields: 'id,caption,media_url,permalink',
+		limit: 3
+	})
+	// console.log(instagramResult)
+
+	// const instagramPosts = await Promise.all(instagramResult.data.map(async instagramResult => {
+
+	// 	const post = {}
+	// 	post.image = await getPhotoData(instagramResult.media_url)
+
+	// 	post.title = null
+	// 	post.caption = instagramResult.caption
+	// 	post.permalink = instagramResult.permalink
+	// 	post.classes = [ 'col-12', 'col-md-4', 'card', 'card-instagram' ]
+	// 	post.source = 'instagram'
+
+	// 	return post
+	// }))
+
+	// Return posts
+	return {props: {
+		studies: studies,
+		projectsPersonal: projects,
+		dribbblePosts: dribbblePosts,
+		// instagramPosts: instagramPosts,
+		// heroImage: await getPhotoData('/images/cc-hero-image-closed.png')
+		heroImage: await getPhotoData('/images/cc-hero-image-open.png')
+	}}
+}
+
+export default ({ studies, projectsPersonal, dribbblePosts, instagramPosts, heroImage }) => {
+
+	const projects = [
+
+		// Case studies
+		{ kicker: 'Curated projects', heading: 'Case studies', posts: studies },
+
+		// Personal projects
+		{kicker: 'Side hussles', heading: 'Personal projects', posts: projectsPersonal },
+
+		// Latest Dribbble shots
+		{ kicker: "What's new", heading: 'Latest on Dribbble', posts: dribbblePosts },
+
+		// Latest Instagram posts
+		// { kicker: 'On socials', heading: 'Latest on Instagram', posts: instagramPosts }
+	]
 
 	return (
 		<div className='container'>
@@ -31,102 +126,32 @@ export default ({ dribbblePosts, igPosts }) => {
 				</div>
 			</div>
 
-			{/* Side contact me button */}
-			<Button
-				href='/contact'
-				className={['side-contact', 'd-none', 'd-md-block']}
-				hasIcon={true}
-				text='Contact me'
-			/>
-	
 			<main>
-				<span className={`text-center ${styles.background}`}>designer</span>
+				<span className={`text-center d-none d-md-block ${styles.background}`}>designer</span>
 
 				{/* Hero Section */}
-				{/* <animated.section className={`row ${styles.hero}`} style={setTransition(heroRef, { x: -200, y: 0, duration: 2000 })} ref={heroRef}> */}
-				<animated.section className={`row ${styles.hero}`} style={setTransition(heroRef)} ref={heroRef}>
-					<div className='col-12 col-md-6 d-flex flex-column justify-content-center'>
-						<Title
-							kicker='Corneliu Cîrlan'
-							heading='Website and user experience designer'
-						/>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero. Integer a mollis augue. Curabitur tristique felis in massa ultrices interdum. Integer tristique ac metus nec molestie. Donec sed bibendum elit, vitae condimentum metus. Suspendisse consequat magna nec ullamcorper iaculis. Fusce magna velit, tempus ultricies finibus a, facilisis vel nisi. Fusce quis ultricies nibh.</p>
-						
-						<div className={`${styles.cta} d-flex justify-content-start align-items-center`}>
-							<Button className={['btn']} href='/contact' text="Let's work together" />
-							<span className='btn-divider'>or</span>
-							<Button className={['btn']} href='/about' text='Read about me' />
-						</div>
-					</div>
-				</animated.section>
+				<Hero
+					hero={styles.hero}
+					cta={styles.cta}
+					image={heroImage}
+					imageClass={styles.image}
+				/>
 
-				{/* Curated Projects Section */}
-				{/* <section className='row'>
-					<Title
-						kicker='Curated Projects'
-						heading='Case studies'
+				{projects.map((project, key) =>
+					<ListProjects
+						key={key}
+						kicker={project.kicker}
+						heading={project.heading}
+						projects={project.posts}
+						source={project.source}
 					/>
-					{dribbblePosts.map((shot, index) =>
-						<Card data={shot} source='dribbble' key={index} />
-						)}
-					</section> */}
-
-				{/* Personal Projects Section */}
-				{/* <section className='row'>
-					<Title
-						kicker='Side Hussles'
-						heading='Personal Projects'
-					/>
-					{dribbblePosts.map((shot, index) =>
-						<Card data={shot} source='dribbble' key={index} />
-						)}
-					</section> */}
-
-				{/* Latest on Dribbble Section */}
-				<section className='row'>
-					<Title
-						kicker="What's New"
-						heading='Latest on Dribbble'
-					/>
-					{dribbblePosts.map((shot, index) =>
-						<Card position={index === 1 ? 'left' : 'right'} data={shot} grid='6' source='dribbble' width='1600' height='1200' key={index} />
-						)}
-				</section>
-
-				{/* Latest on Instagram Section */}
-				<section className='row'>
-					<Title
-						kicker='On Socials'
-						heading='Latest on Instagram'
-					/>
-					{igPosts.data.map((shot, index) =>
-						<Card data={shot} grid='4' source='instagram' width='1080' height='1080' classes=' d-flex justify-content-center align-items-center flex-column' key={index} />
-					)}
-				</section>
+				)}
 
 				{/* Let's work together */}
 				<LetsWorkTogether />
-
 			</main>
 
 			<Footer />
 		</div>
 	)
-}
-
-export async function getStaticProps() {
-
-	const POSTS_DRIBBBLE = 2
-	const POSTS_INSTAGRAM = 3
-
-	// Get latest shots from Dribbble
-	let response = await fetch(`https://api.dribbble.com/v2/user/shots?access_token=${process.env.DRIBBBLE_TOKEN}&per_page=${POSTS_DRIBBBLE}`)
-	let dribbblePosts = await response.json()
-
-	// Get latest IG posts
-	response = await fetch(`https://graph.instagram.com/v14.0/me/media?fields=id,caption,media_url,permalink&limit=${POSTS_INSTAGRAM}&access_token=${process.env.INSTAGRAM_TOKEN}`)
-	let igPosts = await response.json()
-
-	// Return latest shots
-	return {props: {dribbblePosts: dribbblePosts, igPosts: igPosts}}
 }
