3e953188502ce97f80a1c4869c0ea83dc59c7793
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2022-10-01 17:33:59 +0300

    Added photo, services, tools and experience

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,27 @@
+import { useRef } from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Title from '../components/title'
-import Button from '../components/button'
 import LetsWorkTogether from '../components/work-together'
-import { setTransition } from '../utils/transitions'
-
-import { animated } from 'react-spring'
+import Caption from '../components/caption'
+import { getPhotoData } from '../utils/images'
 
 import styles from '../sass/modules/About.module.sass'
-import { useRef } from 'react'
 
-export default () => {
+export const getStaticProps = async () => {
+
+	return {
+		props: {
+			aboutMePhoto: await getPhotoData('/images/cc-about.jpg')
+		}
+	}
+}
+
+
+export default ({ aboutMePhoto }) => {
 
 	const aboutRef = useRef()
 	const photoRef = useRef()
@@ -21,41 +30,25 @@ export default () => {
 	const experienceRef = useRef()
 	const experienceListRef = useRef()
 
-	const services = [
-		'Art direction',
-		'Web & mobile',
-		'Brand identity',
-		'UX & UI',
-		'Iconography',
-		'Animation',
-		'Photography'
-	]
-	const tools = [
-		'Photoshop',
-		'Illustrator',
-		'XD',
-		'Dimension',
-		'After Effects',
-		'Lightroom',
-		'Figma',
-		'Visual Studio Code'
-	]
-	const experience = [
+	const servicesAndTools = [
 		{
-			name: 'Corneliu Cîrlan PFA',
-			title: 'Founder, Creative Director, Freelancer, Designer, Developer',
-			period: 'February 2013 - Present'
+			title: 'Services',
+			items: [ 'Art direction', 'Web & mobile', 'Brand identity', 'UX & UI', 'Iconography', 'Animation', 'Photography' ],
+			classes: `col-12 offset-md-1 col-md-4 ${styles.services}`,
+			ref: servicesRef
 		},
 		{
-			name: 'Uncover Romania Tours',
-			title: 'Full-stack designer',
-			period: 'February 2015 - April 2016'
-		},
-		{
-			name: 'Uncover Romania',
-			title: 'Co-founder, Web, UX / UI Designer, Developer',
-			period: 'January 2012 - April 2016'
+			title: 'Tools',
+			items: [ 'Photoshop', 'Illustrator', 'XD', 'Dimension', 'After Effects', 'Lightroom', 'Figma', 'Visual Studio Code' ],
+			classes: 'col-12 offset-md-2 col-md-4',
+			ref: toolsRef
 		}
+	]
+
+	const experience = [
+		{ name: 'Corneliu Cîrlan PFA', title: 'Founder, Creative Director, Freelancer, Designer, Developer', period: 'February 2013 - Present' },
+		{ name: 'Uncover Romania Tours', title: 'Full-stack designer', period: 'February 2015 - April 2016' },
+		{ name: 'Uncover Romania', title: 'Co-founder, Web, UX / UI Designer, Developer', period: 'January 2012 - April 2016' }
 	]
 
 	return (
@@ -66,64 +59,56 @@ export default () => {
 
 			<Header />
 
-			{/* Side contact me button */}
-			<Button
-				href='/contact'
-				className={['side-contact', 'd-none', 'd-md-block']}
-				hasIcon={true}
-				text='Contact me'
-			/>
-
 			<main className={`text-center ${styles.about}`}>
 
 				{/* About me */}
-				<animated.section className='row' style={setTransition(aboutRef)} ref={aboutRef}>
+				<section className={`row ${useIsInViewport(aboutRef) ? 'animate-in' : null}`} ref={aboutRef}>
 					<div className='col-12 offset-md-2 col-md-8'>
-						<Title
+						<Caption
 							kicker='A little about me'
 							heading="Hi, I'm Corneliu Cîrlan, the designer you're looking for"
+							caption='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero.'
 						/>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero. Integer a mollis augue. Curabitur tristique felis in massa ultrices interdum. Integer tristique ac metus nec molestie. Donec sed bibendum elit, vitae condimentum metus. Suspendisse consequat magna nec ullamcorper iaculis. Fusce magna velit, tempus ultricies finibus a, facilisis vel nisi. Fusce quis ultricies nibh.</p>
 					</div>
-
-				</animated.section>
+				</section>
 
 				{/* Photo */}
-				<animated.section className='row' style={setTransition(photoRef)} ref={photoRef}>
-					<div className={`col-12 offset-md-1 col-md-10 ${styles.photo}`}></div>
-				</animated.section>
+				<section className={`row ${useIsInViewport(photoRef) ? 'animate-in' : null}`} ref={photoRef}>
+					{/* <div className={`col-12 offset-md-1 col-md-10 ${styles.photo}`}></div> */}
+
+					<Image
+						src={aboutMePhoto.src}
+						width={aboutMePhoto.width}
+						height={aboutMePhoto.height}
+						placeholder='blur'
+						blurDataURL={aboutMePhoto.base64}
+						priority={true}
+					/>
+				</section>
 
 				{/* Services & tools */}
 				<section className='row'>
-
-					{/* Services */}
-					<animated.div className={`col-12 offset-md-1 col-md-4 ${styles.services}`} style={setTransition(servicesRef, { x: -20, y: 0 })} ref={servicesRef}>
-						<Title kicker='Services' />
-						<ul className={styles.list}>
-							{services.map((service, index) => <li key={index} className={styles.item}>{service}</li>)}
-						</ul>
-					</animated.div>
-					
-					{/* Tools */}
-					<animated.div className='col-12 offset-md-2 col-md-4' style={setTransition(toolsRef, { x: 20, y: 0 })} ref={toolsRef}>
-						<Title kicker='Tools I use' />
-						<ul className={styles.list}>
-							{tools.map((tool, index) => <li key={index} className={styles.item}>{tool}</li>)}
-						</ul>
-					</animated.div>
+					{servicesAndTools.map((element, key) =>
+						<div key={key} className={element.classes} ref={element.ref}>
+							<Title kicker={element.title} />
+							<ul className={styles.list}>
+								{element.items.map((item, index) => <li key={index} className={styles.item}>{item}</li>)}
+							</ul>	
+						</div>
+					)}
 				</section>
 
 				{/* Experience */}
 				<section className='row'>
-					<animated.div className='col-12 offset-md-2 col-md-8' style={setTransition(experienceRef)} ref={experienceRef}>
-						<Title
+					<div className={`col-12 offset-md-2 col-md-8 ${useIsInViewport(experienceRef) ? 'animate-in' : null}`} ref={experienceRef}>
+						<Caption
 							kicker='Experience'
-							heading='Over 10 years of experience'
-							/>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero. Integer a mollis augue. Curabitur tristique felis in massa ultrices interdum. Integer tristique ac metus nec molestie. Donec sed bibendum elit, vitae condimentum metus. Suspendisse consequat magna nec ullamcorper iaculis. Fusce magna velit, tempus ultricies finibus a, facilisis vel nisi. Fusce quis ultricies nibh.</p>
-					</animated.div>
+							heading="Over 10 years of experience"
+							caption='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero.'
+						/>
+					</div>
 
-					<animated.div className={`col-12 offset-md-1 col-md-10 ${styles.experience}`} style={setTransition(experienceListRef)} ref={experienceListRef}>
+					<div className={`col-12 offset-md-1 col-md-10 ${styles.experience} ${useIsInViewport(experienceListRef) ? 'animate-in' : null}`} ref={experienceListRef}>
 						{experience.map((item, index) =>
 							<div key={index} className={`d-flex justify-content-between flex-column flex-md-row ${styles.item}`}>
 								<span>{item.name}</span>
@@ -131,7 +116,7 @@ export default () => {
 								<span>{item.period}</span>
 							</div>
 						)}
-					</animated.div>
+					</div>
 				</section>
 
 				{/* Let's work together */}
