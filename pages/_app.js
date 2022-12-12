38b0d93d6bbc9a26b6cf7a9ae571480dbc06d0fd
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2022-12-12 16:38:12 +0000

    Changed to lower case components  filenames

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import Footer from '../components/footer'
 
+import Footer from '../components/footer'
 import Header from '../components/header'
 
 // Global SASS
