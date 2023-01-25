a78d3de93b32a7bfcc3e42d6df56875aa6c7afcc
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-01-25 17:36:07 +0200

    Personal projects section done

--- components/data/projects.js
+++ components/data/projects.js
@@ -1,77 +1,68 @@
 export default [
+
+	// Uncover Romania
 	{
-		type: 'study',
-		imageURL: '/images/projects/uncover-romania/1.webp',
-		case: 'uncover-romania',
-		title: 'Uncover Romania',
-		caption: 'caption text',
-		permalink: '/uncover-romania',
-		classes: [ 'col-12', 'col-md-6', 'card' ],
+		type: "study",
+		imageURL: "/images/projects/uncover-romania/1.webp",
+		id: "uncover-romania",
+		title: "Uncover Romania",
+		caption: "caption text",
+		permalink: "/uncover-romania",
 		details: [
 			{
-				title: 'My role',
-				text: [ 'Creative Direction', 'UX / UI', 'Website Design', 'Web Development' ]
+				title: "My role",
+				text: [
+					"Creative Direction",
+					"UX / UI",
+					"Website Design",
+					"Web Development",
+				],
 			},
 			{
-				title: 'Client',
-				text: 'Client name'
+				title: "Client",
+				text: "Client name",
 			},
 			{
-				title: 'Period',
-				text: '2012 - 2016'
-			}
+				title: "Period",
+				text: "2012 - 2016",
+			},
 		],
 		source: null,
-		caption: 'Lorem ipsum dolor sit amed. #one #two',
+		caption: "Lorem ipsum dolor sit amed. #one #two",
 		description: [
 			"Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras pretium maximus nunc. Duis vitae tincidunt purus. Donec scelerisque tortor at lacinia sollicitudin. Etiam interdum nulla scelerisque, eleifend justo quis, hendrerit leo. Vestibulum sed justo lacinia, finibus augue at, feugiat risus. Cras sed mi elit. Maecenas pellentesque nunc nec viverra gravida. Vivamus ut efficitur enim. Nunc et ex leo. Vestibulum vel nibh scelerisque, ultricies tortor gravida, lacinia neque. In vehicula vitae lorem nec posuere.",
 			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero. Integer a mollis augue. Curabitur tristique felis in massa ultrices interdum. Integer tristique ac metus nec molestie. Donec sed bibendum elit, vitae condimentum metus. Suspendisse consequat magna nec ullamcorper iaculis. Fusce magna velit, tempus ultricies finibus a, facilisis vel nisi. Fusce quis ultricies nibh. Nam et mauris ac massa ultricies malesuada. Aliquam congue in mauris eu lobortis.",
-			"Phasellus pellentesque metus sed felis sodales maximus. Nullam ac ex vel odio suscipit vehicula porta ut neque. In ut nunc eget augue pretium molestie. Sed interdum, ligula vel luctus elementum, elit est scelerisque ante, eu venenatis nisl arcu non ante. Nunc fringilla varius nisi. Integer congue id mi dignissim hendrerit. Curabitur non erat mauris. Integer sit amet nisi sit amet ex suscipit dictum. Proin pretium nisi vitae ex maximus, eget pulvinar mi tempus. Sed finibus, elit non elementum venenatis, metus dui pretium purus, vel euismod turpis nulla vitae massa. Vivamus in arcu tempus, dapibus felis a, vestibulum leo. Aliquam erat volutpat. Aliquam eu tempus turpis, ut efficitur orci. Phasellus vitae vulputate purus. In a diam sed tellus dignissim varius sed at metus."
+			"Phasellus pellentesque metus sed felis sodales maximus. Nullam ac ex vel odio suscipit vehicula porta ut neque. In ut nunc eget augue pretium molestie. Sed interdum, ligula vel luctus elementum, elit est scelerisque ante, eu venenatis nisl arcu non ante. Nunc fringilla varius nisi. Integer congue id mi dignissim hendrerit. Curabitur non erat mauris. Integer sit amet nisi sit amet ex suscipit dictum. Proin pretium nisi vitae ex maximus, eget pulvinar mi tempus. Sed finibus, elit non elementum venenatis, metus dui pretium purus, vel euismod turpis nulla vitae massa. Vivamus in arcu tempus, dapibus felis a, vestibulum leo. Aliquam erat volutpat. Aliquam eu tempus turpis, ut efficitur orci. Phasellus vitae vulputate purus. In a diam sed tellus dignissim varius sed at metus.",
 		],
-		behanceURL: 'https://www.behance.net/corneliucirlan',
-		target: '_blank',
+		behanceURL: "https://www.behance.net/corneliucirlan",
+		target: "_blank",
 		images: [
-			'1.webp',
-			'2.webp',
-			'3.webp',
-			'4.webp',
-			'5.webp',
-			'6.webp',
-			'7.webp',
-			'8.webp' 
+			"1.webp",
+			"2.webp",
+			"3.webp",
+			"4.webp",
+			"5.webp",
+			"6.webp",
+			"7.webp",
+			"8.webp",
 		],
 	},
 
+	// MDSM
 	{
-		type: 'personal',
-		imageURL: '/images/projects/mdsm/cover.png',
-		case: 'mdsm',
-		title: 'MDSM',
-		caption: `A jQuery | JavaScript plugin built on top of Bootstrap’s framework to create a mobile responsive menu similar to Google’s Material Design.`,
-		permalink: '/mdsm',
-		classes: [ 'col-12', 'col-md-6', 'card' ],
-		source: null,
-		role: [
-			'Creative Direction', 'UX / UI', 'Website Design', 'Web Development'
-		],
-		// client: 'Client name',
-		period: '2021 - present',
-		description: [
-			"Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras pretium maximus nunc. Duis vitae tincidunt purus. Donec scelerisque tortor at lacinia sollicitudin. Etiam interdum nulla scelerisque, eleifend justo quis, hendrerit leo. Vestibulum sed justo lacinia, finibus augue at, feugiat risus. Cras sed mi elit. Maecenas pellentesque nunc nec viverra gravida. Vivamus ut efficitur enim. Nunc et ex leo. Vestibulum vel nibh scelerisque, ultricies tortor gravida, lacinia neque. In vehicula vitae lorem nec posuere.",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus ipsum, malesuada sed volutpat id, dignissim vitae quam. Maecenas nibh leo, laoreet eget nisi ac, sagittis imperdiet libero. Integer a mollis augue. Curabitur tristique felis in massa ultrices interdum. Integer tristique ac metus nec molestie. Donec sed bibendum elit, vitae condimentum metus. Suspendisse consequat magna nec ullamcorper iaculis. Fusce magna velit, tempus ultricies finibus a, facilisis vel nisi. Fusce quis ultricies nibh. Nam et mauris ac massa ultricies malesuada. Aliquam congue in mauris eu lobortis.",
-			"Phasellus pellentesque metus sed felis sodales maximus. Nullam ac ex vel odio suscipit vehicula porta ut neque. In ut nunc eget augue pretium molestie. Sed interdum, ligula vel luctus elementum, elit est scelerisque ante, eu venenatis nisl arcu non ante. Nunc fringilla varius nisi. Integer congue id mi dignissim hendrerit. Curabitur non erat mauris. Integer sit amet nisi sit amet ex suscipit dictum. Proin pretium nisi vitae ex maximus, eget pulvinar mi tempus. Sed finibus, elit non elementum venenatis, metus dui pretium purus, vel euismod turpis nulla vitae massa. Vivamus in arcu tempus, dapibus felis a, vestibulum leo. Aliquam erat volutpat. Aliquam eu tempus turpis, ut efficitur orci. Phasellus vitae vulputate purus. In a diam sed tellus dignissim varius sed at metus."
-		],
-		behanceURL: 'https://mdsm.corneliucirlan.com',
-		target: '_blank',
-		images: [
-			// '1.webp',
-			// '2.webp',
-			// '3.webp',
-			// '4.webp',
-			// '5.webp',
-			// '6.webp',
-			// '7.webp',
-			// '8.webp' 
-		]
-	}
+		type: "personal",
+		imageURL: "/images/projects/mdsm.png",
+		title: "MDSM",
+		caption: `A plugin for jQuery or JavaScript that utilizes Bootstrap's framework to create a mobile-responsive menu in the style of Google's Material Design.`,
+		permalink: "https://mdsm.corneliucirlan.com",
+	},
+
+	// Stream
+	{
+		type: "personal",
+		imageURL: "/images/projects/stream.png",
+		title: "Global Streaming Search for Movies and TV Shows",
+		caption: `A web app designed for easy searching of global streaming options for movies and TV shows.`,
+		permalink: "https://stream.corneliucirlan.com",
+	},
 ]
