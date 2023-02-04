f03fba6839c4cdb76ed0bc4bacfc4e0ec8fce61f
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-02-04 11:04:10 +0200

    Push current route before scrolling

--- components/scroll-top.js
+++ components/scroll-top.js
@@ -1,11 +1,12 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { usePathname } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 
 export default () => {
 	// Current path
 	const pathname = usePathname()
+	const router = useRouter()
 
 	// Previous path
 	const [oldPathName, setOldPathName] = useState()
@@ -13,6 +14,7 @@ export default () => {
 	useEffect(() => {
 		// Scroll to top if current path not as previous path
 		if (oldPathName !== pathname) {
+			router.push(pathname)
 			window.scrollTo(0, 0)
 			setOldPathName(pathname)
 		}
