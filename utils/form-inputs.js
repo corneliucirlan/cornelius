c1a68270450d040b064fb1a5292e497073c675ec
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-02-07 18:10:23 +0200

    Fixed inputs not clearing after form submit

--- utils/form-inputs.js
+++ utils/form-inputs.js
@@ -1,53 +1,94 @@
-import Select from 'react-select'
+import Select from "react-select"
 
-const isInvalidClass = 'is-invalid'
+const isInvalidClass = "is-invalid"
 
 // Input
-export const FormInput = ({ forLabel, type, name, value, classes, placeholder, setValue }) => {
-
+export const FormInput = ({
+	forLabel,
+	type,
+	name,
+	value,
+	classes,
+	placeholder,
+	setValue
+}) => {
 	return (
-		<div className='col-12 col-md-6'>
-			<label htmlFor={name} className='form-label'>{forLabel}</label>
+		<div className="col-12 col-md-6">
+			<label htmlFor={name} className="form-label">
+				{forLabel}
+			</label>
 			<input
 				type={type}
 				name={name}
-				defaultValue={value}
+				value={value}
 				className={classes}
 				placeholder={placeholder}
-				onChange={event => setValue(event.target.value)}
-				onFocus={event => event.target.classList.remove(isInvalidClass)}
-				onBlur={event => !event.target.value && event.target.classList.add(isInvalidClass)}
-				/>
+				onChange={(event) => setValue(event.target.value)}
+				onFocus={(event) =>
+					event.target.classList.remove(isInvalidClass)
+				}
+				onBlur={(event) =>
+					!event.target.value &&
+					event.target.classList.add(isInvalidClass)
+				}
+			/>
 		</div>
 	)
 }
 
 // Textarea
-export const FormTextare = ({ forLabel, name, value, classes, placeholder, setValue }) => {
-	
+export const FormTextare = ({
+	forLabel,
+	name,
+	value,
+	classes,
+	placeholder,
+	setValue
+}) => {
 	return (
-		<div className='col-12'>
-			<label htmlFor={name} className='form-label'>{forLabel}</label>
+		<div className="col-12">
+			<label htmlFor={name} className="form-label">
+				{forLabel}
+			</label>
 			<textarea
-				rows='1'
+				rows="1"
 				name={name}
-				defaultValue={value}
+				value={value}
 				className={classes}
 				placeholder={placeholder}
-				onChange={e => {setValue(e.target.value)}}
-				onFocus={event => event.target.classList.remove(isInvalidClass)}
-				onBlur={event => !event.target.value && event.target.classList.add(isInvalidClass)}
+				onChange={(e) => {
+					setValue(e.target.value)
+				}}
+				onFocus={(event) =>
+					event.target.classList.remove(isInvalidClass)
+				}
+				onBlur={(event) =>
+					!event.target.value &&
+					event.target.classList.add(isInvalidClass)
+				}
 			/>
 		</div>
 	)
 }
 
 // Select
-export const FormSelect = ({ forLabel, name, placeholder, classes, namePrefix, styles, value, id, options,setValue }) => {
-
+export const FormSelect = ({
+	forLabel,
+	name,
+	placeholder,
+	classes,
+	namePrefix,
+	styles,
+	value,
+	id,
+	options,
+	setValue
+}) => {
 	return (
-		<div className='col-12 col-md-6'>
-			<label htmlFor={name} className='form-label'>{forLabel}</label>
+		<div className="col-12 col-md-6">
+			<label htmlFor={name} className="form-label">
+				{forLabel}
+			</label>
 			<Select
 				name={name}
 				placeholder={placeholder}
@@ -58,9 +99,21 @@ export const FormSelect = ({ forLabel, name, placeholder, classes, namePrefix, s
 				id={id}
 				instanceId={id}
 				options={options}
-				onChange={event => setValue(event)}
-				onFocus={event => event.target.closest('.form-control').classList.remove(isInvalidClass)}
-				onBlur={event => !value ? event.target.closest('.form-control').classList.add(isInvalidClass) : event.target.closest('.form-control').classList.remove(isInvalidClass)}
+				onChange={(event) => setValue(event)}
+				onFocus={(event) =>
+					event.target
+						.closest(".form-control")
+						.classList.remove(isInvalidClass)
+				}
+				onBlur={(event) =>
+					!value
+						? event.target
+								.closest(".form-control")
+								.classList.add(isInvalidClass)
+						: event.target
+								.closest(".form-control")
+								.classList.remove(isInvalidClass)
+				}
 			/>
 		</div>
 	)
