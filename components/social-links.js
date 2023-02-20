c17ea50bb5cdfe610b06b7a598508f97e344ad58
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2023-02-20 10:38:26 +0200

    Changed social links to icons

---
+++ components/social-links.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import { socialLinks } from "./data/menus"
+
+export default () => {
+	return (
+		<div className="d-flex justify-content-between">
+			{socialLinks.map((link, key) => (
+				<Link
+					key={key}
+					aria-label={link.title}
+					href={link.url}
+					target={link.target}
+					rel={link.rel}
+				>
+					{link.svg}
+				</Link>
+			))}
+		</div>
+	)
+}
